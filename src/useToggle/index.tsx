7c266cc7a2427dd15dd2ef95729d347d86b5cbd0
Tejas-CureFit <tejas.upmanyu@v.curefit.com>
2020-05-14 19:48:49 +0530

    fixed types for useToggle

--- src/useToggle/index.tsx
+++ src/useToggle/index.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
+
 /**
  * useToggle returns a toggle-able state and corresponding toggler function.
  * @param initialValue for the initial value of toggle.
  */
-export const useToggle = (initialValue?: boolean) => {
-  const [value, setValue] = React.useState(!!initialValue);
+export const useToggle = (
+  initialValue?: boolean
+): [boolean, (target?: boolean) => void] => {
+  const [value, setValue] = React.useState<boolean>(initialValue || false);
 
   const toggle: (target?: boolean) => void = (target?: boolean) => {
     if (target !== undefined) {
